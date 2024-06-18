83daae1d52a932d95dd577ecd14aa75332212210
ibrahimgedami <ibrahim.abdelgani@seddiqiholding.com>
2024-06-18 11:35:16 +0400

    commit

--- Sources/SearchableTableView/CustomeDropDownList/Configuration.swift
+++ Sources/SearchableTableView/CustomeDropDownList/Configuration.swift
@@ -9,12 +9,11 @@ import UIKit
 
 public enum DropDownListConfiguration<DataModel: Displayable & Searchable> {
     
-    case search([DataModel])
+    case search([DataModel], (((any Searchable)?) -> ())?)
     
     public var viewController: UIViewController {
         switch self {
-        case .search(let array):
-            var completion: (((any Searchable)?) -> ())?
+        case .search(let array, let completion):
             let viewModel = DropDownListViewModel(dataSource: array)
             let viewController = DropDownListViewController(viewModel: viewModel, closure: completion)
             return viewController
